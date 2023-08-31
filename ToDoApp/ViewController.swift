0fc4577e06d1028a6fd08a70018c819cfb688acc
Bucky <bucky5683@gmail.com>
2023-08-31 14:44:57 +0900

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by 김서연 on 2023/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
